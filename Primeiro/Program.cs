74f3436fc472ad03e4dd85337bc7c28557415441
rafael-sander <rafael.sander@gmail.com>
2022-02-15 17:40:54 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World!";
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
